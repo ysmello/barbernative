e910a40bc056bc9097200d1384f4efed1eeccefd
Yuri Mello <yuridemeello@gmail.com>
2020-08-26 23:48:41 -0300

    Components input e button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container, Title } from './styles'
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
-      <Image source={logoImg}/>
+      <Image source={logoImg} />
 
       <Title>Faça seu logon</Title>
-    </Container>
-  )
-}
 
-export default SignIn
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button onPress={() => console.log('testes')}>Entrar</Button>
+    </Container>
+  );
+};
+
+export default SignIn;
